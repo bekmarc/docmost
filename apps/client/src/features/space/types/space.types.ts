d77e86f712d7fb24997c3d6e24815f2c1ba0c2cf
bekmarc <bekoumarco123@gmail.com>
2024-11-18 10:46:36 +0000

    install

--- apps/client/src/features/space/types/space.types.ts
+++ apps/client/src/features/space/types/space.types.ts
@@ -1,70 +1,78 @@
-import { SpaceRole } from "@/lib/types.ts";
-import {
-  SpaceCaslAction,
-  SpaceCaslSubject,
-} from "@/features/space/permissions/permissions.type.ts";
+import {SpaceCaslAction, SpaceCaslSubject,} from "@/features/space/permissions/permissions.type.ts";
+import {SpaceRole} from "@/lib/types.ts";
 
 export interface ISpace {
-  id: string;
-  name: string;
-  description: string;
-  icon: string;
-  slug: string;
-  hostname: string;
-  creatorId: string;
-  createdAt: Date;
-  updatedAt: Date;
-  memberCount?: number;
-  spaceId?: string;
-  membership?: IMembership;
+    id: string;
+    name: string;
+    description: string;
+    icon: string;
+    slug: string;
+    hostname: string;
+    creatorId: string;
+    createdAt: Date;
+    updatedAt: Date;
+    memberCount?: number;
+    spaceId?: string;
+    membership?: IMembership;
 }
 
 interface IMembership {
-  userId: string;
-  role: SpaceRole;
-  permissions?: Permissions;
+    userId: string;
+    role: SpaceRole;
+    permissions?: Permissions;
 }
 
 interface Permission {
-  action: SpaceCaslAction;
-  subject: SpaceCaslSubject;
+    action: SpaceCaslAction;
+    subject: SpaceCaslSubject;
 }
+
+const permissionXample: Permissions = [
+    {
+        action: SpaceCaslAction.Manage,
+        subject: SpaceCaslSubject.Page
+    },
+    {
+        action: SpaceCaslAction.Manage,
+        subject: SpaceCaslSubject.Member
+    },
+]
 
 type Permissions = Permission[];
 
 export interface IAddSpaceMember {
-  spaceId: string;
-  userIds?: string[];
-  groupIds?: string[];
+    spaceId: string;
+    userIds?: string[];
+    groupIds?: string[];
 }
 
 export interface IRemoveSpaceMember {
-  spaceId: string;
-  userId?: string;
-  groupId?: string;
+    spaceId: string;
+    userId?: string;
+    groupId?: string;
 }
 
 export interface IChangeSpaceMemberRole {
-  spaceId: string;
-  userId?: string;
-  groupId?: string;
+    spaceId: string;
+    userId?: string;
+    groupId?: string;
 }
 
 // space member
 export interface SpaceUserInfo {
-  id: string;
-  name: string;
-  email: string;
-  avatarUrl: string;
-  type: "user";
+    id: string;
+    name: string;
+    email: string;
+    avatarUrl: string;
+    type: "user";
 }
 
 export interface SpaceGroupInfo {
-  id: string;
-  name: string;
-  isDefault: boolean;
-  memberCount: number;
-  type: "group";
+    id: string;
+    name: string;
+    isDefault: boolean;
+    memberCount: number;
+    type: "group";
 }
 
 export type ISpaceMember = { role: string } & (SpaceUserInfo | SpaceGroupInfo);
